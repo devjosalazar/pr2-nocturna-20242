5e43e6c22f480bb4683aa05aa5d146f8bc78b01c
josalazar <53492638+devjosalazar@users.noreply.github.com>
2024-10-29 20:54:23 -0500

    Add files patron estructurales

---
+++ co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/programacion2/patrones_estructurales/facade/bridge/ejemploDispositivo/MainBridge.java
@@ -0,0 +1,21 @@
+package co.edu.uniquindio.programacion2.patrones_estructurales.bridge.ejemploDispositivo;
+
+
+import co.edu.uniquindio.programacion2.patrones_estructurales.bridge.ejemploDispositivo.abstraccion.Pedido;
+import co.edu.uniquindio.programacion2.patrones_estructurales.bridge.ejemploDispositivo.abstraccion.PedidoDigital;
+import co.edu.uniquindio.programacion2.patrones_estructurales.bridge.ejemploDispositivo.abstraccion.PedidoFisico;
+import co.edu.uniquindio.programacion2.patrones_estructurales.bridge.ejemploDispositivo.implementacion.*;
+
+public class MainBridge {
+    // Uso del patrón Bridge
+    public static void main(String[] args) {
+        ISistemaPago pagoTarjeta = new PagoTarjeta();
+        Pedido pedidoFisico = new PedidoFisico(pagoTarjeta);
+        pedidoFisico.realizarPago(100.0);
+
+        ISistemaPago pagoConPayPal = new PagoPaypal();
+        Pedido pedidoDigital = new PedidoDigital(pagoConPayPal);
+        pedidoDigital.realizarPago(50.0);
+    }
+
+}
